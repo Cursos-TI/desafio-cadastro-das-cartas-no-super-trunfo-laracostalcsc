100b137ff850cd6ca57ea004d57b37209938b2ae
laracostalcsc <laracosta.lcsc@gmail.com>
2025-02-16 21:34:41 +0000

    CARTA DO PAIS CHILE.c

---
+++ CARTA DO PAIS CHILE.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    printf("CARTA DO PAIS: CHILE \n");
+    printf("CAPITAL: SANTIAGO\n");
+
+    printf("ÁREA: 756.102 km²\n");
+
+    printf("POPULAÇÃO: 19.116.209 hab\n");
+
+    printf("DENSIDADE DEMOGRÁFICA: 25,71 hab/km²\n");
+    printf("IDH: 0,851\n");
+    printf("PIB: US$ 282,31 bi\n");
+    printf("MOEDA: Peso\n");
+    printf("Número de pontos turísticos 15\n");
+    printf("\n");
+
+ 
+
+
+    return 0;
+
+}
