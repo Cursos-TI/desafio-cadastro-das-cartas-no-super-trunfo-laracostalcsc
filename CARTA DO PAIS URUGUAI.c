e8c3f8175c24fb11566f06dacfc57676c85c80c7
laracostalcsc <laracosta.lcsc@gmail.com>
2025-02-16 21:16:24 +0000

    CARTA DO PAIS URUGUAI.c

---
+++ CARTA DO PAIS URUGUAI.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    printf("CARTA DO PAIS: URUGUAI \n");
+    printf("CAPITAL: MONTEVIDÉU\n");
+
+    printf("ÁREA: 176.215 km²\n");
+
+    printf("POPULAÇÃO: 3.473.727 hab\n");
+
+    printf("DENSIDADE DEMOGRÁFICA: 19,85 hab/km²\n");
+    printf("IDH: 0,817\n");
+    printf("PIB: US$ 56,04 bi\n");
+    printf("MOEDA: Peso uruguaio\n");
+    printf("Número de pontos turísticos 30\n");
+    printf("\n");
+
+ 
+
+
+    return 0;
+
+}
