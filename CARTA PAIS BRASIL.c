f6e35be1686fb7283a11305c89828526ab607386
laracostalcsc <laracosta.lcsc@gmail.com>
2025-02-16 20:03:04 +0000

    CARTA PAIS BRASIL

---
+++ CARTA PAIS BRASIL.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("CARTA DO PAIS: BRASIL\n");
+    printf("CAPITAL: BRASÍLIA\n");
+
+    printf("ÁREA: 8.510.345 km²\n");
+
+    printf("POPULAÇÃO: 212.559.409 hab\n");
+
+    printf("DENSIDADE DEMOGRÁFICA: 25,43 hab/km²\n");
+    printf("IDH: 0,765\n");
+    printf("PIB: US$ 1,84 tri\n");
+    printf("MOEDA: Real\n");
+    printf("Número de pontos turísticos:200\n");
+    return 0;
+}
