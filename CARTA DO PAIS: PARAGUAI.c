e6767372faa30f03d4a90f3e02fdeb46edb18709
laracostalcsc <laracosta.lcsc@gmail.com>
2025-02-16 21:02:08 +0000

    CARTA DO PAIS PARAGUAI.c

---
+++ CARTA DO PAIS: PARAGUAI.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    printf("CARTA DO PAIS: PARAGUAI \n");
+    printf("CAPITAL: ASSUNÇÃO\n");
+
+    printf("ÁREA: 406.752 km²\n");
+
+    printf("POPULAÇÃO: 7.132.530 hab\n");
+
+    printf("DENSIDADE DEMOGRÁFICA: 17,95 hab/km²\n");
+    printf("IDH: 0,728PIB:\n");
+    printf("PIB: US$ 38,08 bi\n");
+    printf("MOEDA: Guarani\n");
+    printf("Número de pontos turísticos 10\n");
+    printf("\n");
+
+ 
+
+
+    return 0;
+
+}
