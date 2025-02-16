710478c7785b4b110df8c5bbad0ac845772efa32
laracostalcsc <laracosta.lcsc@gmail.com>
2025-02-16 21:40:16 +0000

    CARTA DO PAIS BOLÍVIA.c

---
+++ CARTA DO PAIS BOLÍVIA.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    printf("CARTA DO PAIS: BOLÍVIA \n");
+    printf("CAPITAL: LA PAZ\n");
+
+    printf("ÁREA: 1.098.581 km²\n");
+
+    printf("POPULAÇÃO: 11.673.029 hab\n");
+
+    printf("DENSIDADE DEMOGRÁFICA: 10,78hab/km²\n");
+    printf("IDH: 0,718\n");
+    printf("PIB: US$ 40,89 bi\n");
+    printf("MOEDA: Boliviano\n");
+    printf("Número de pontos turísticos: 5\n");
+    printf("\n");
+
+ 
+
+
+    return 0;
+
+}
